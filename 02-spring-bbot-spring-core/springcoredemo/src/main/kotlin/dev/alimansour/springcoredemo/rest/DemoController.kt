8d6ed3f5886a63dc380049965dbb8be2d78c3824
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-13 15:54:50 +0300

    chore: Add examples of setter and field injection
    - Add commented-out code in `DemoController.kt` to demonstrate how to use setter injection and field injection with the `@Autowired` annotation.

--- 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/rest/DemoController.kt
+++ 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/rest/DemoController.kt
@@ -8,6 +8,24 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 
 class DemoController @Autowired constructor(private val coach: Coach) {
+
+    /*
+    // Setter Injection
+    @Autowired
+    private lateinit var coach: Coach
+
+    @Autowired
+    fun setCoach2(coach: Coach) {
+        this.coach = coach
+    }
+    */
+
+    /*
+    // Field Injection
+    @Autowired
+    private lateinit var coach: Coach
+    */
+
     @GetMapping("/dailyworkout")
     fun getDailyWorkout(): String {
         return coach.getDailyWorkout()
