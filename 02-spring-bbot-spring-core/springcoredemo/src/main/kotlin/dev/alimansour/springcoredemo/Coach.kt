554becc17a411714ee5d016fc19fdee0e0720ce9
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-13 14:50:13 +0300

    feat: Add Coach interface and CricketCoach implementation
    - Add a `Coach` interface with a `getDailyWorkout` method.
    - Add a `CricketCoach` class that implements the `Coach` interface.
    - Annotate the `CricketCoach` class with `@Component` to register it as a Spring bean.

---
+++ 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/Coach.kt
@@ -0,0 +1,5 @@
+package dev.alimansour.springcoredemo
+
+interface Coach {
+    fun getDailyWorkout(): String
+}
---
+++ 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/CricketCoach.kt
@@ -0,0 +1,10 @@
+package dev.alimansour.springcoredemo
+
+import org.springframework.stereotype.Component
+
+@Component
+class CricketCoach : Coach {
+    override fun getDailyWorkout(): String {
+        return "Practice fast bowling for 15 minutes"
+    }
+}
