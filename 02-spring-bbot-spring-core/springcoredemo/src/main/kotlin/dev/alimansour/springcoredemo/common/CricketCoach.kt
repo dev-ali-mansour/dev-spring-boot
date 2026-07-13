db2c5bd8f2b1d6edc9f40dfae72023f1e2c20f1a
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-13 17:32:37 +0300

    feat: Add bean lifecycle methods to CricketCoach
    - Add `@PostConstruct` and `@PreDestroy` annotations to `CricketCoach`.
    - Implement `doMyStartupStaff()` and `doMyCleanupStuff()` methods to log bean initialization and destruction.

--- 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/common/CricketCoach.kt
+++ 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/common/CricketCoach.kt
@@ -1,5 +1,7 @@
 package dev.alimansour.springcoredemo.common
 
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
 import org.springframework.stereotype.Component
 
 @Component
@@ -7,6 +9,18 @@ import org.springframework.stereotype.Component
 class CricketCoach : Coach {
     init {
         println("In constructor: ${javaClass.simpleName}")
+    }
+
+    // define our initializing method
+    @PostConstruct
+    fun doMyStartupStaff() {
+        println("In doMyStartupStuff(): ${javaClass.simpleName}")
+    }
+
+    // define our destroy method
+    @PreDestroy
+    fun doMyCleanupStuff() {
+        println("In doMyCleanupStuff(): ${javaClass.simpleName}")
     }
 
     override fun getDailyWorkout(): String {
