04b63066b152d7b8ea1fa547f0dce7644e4b9a5c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-13 16:24:17 +0300

    feat: Set TrackCoach as the primary bean
    - Add the `@Primary` annotation to the `TrackCoach` class.

--- 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/common/TrackCoach.kt
+++ 02-spring-bbot-spring-core/springcoredemo/src/main/kotlin/dev/alimansour/springcoredemo/common/TrackCoach.kt
@@ -1,8 +1,10 @@
 package dev.alimansour.springcoredemo.common
 
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 
 @Component
+@Primary
 class TrackCoach : Coach {
     override fun getDailyWorkout(): String {
         return "Run a hard 5k!"
