8d6290d00c881e910fe9da68f54aef8c54565ffd
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-16 17:55:40 +0300

    refactor: Move student data initialization to @PostConstruct
    - Define a class-level `students` list to store student data.
    - Add a `loadData()` method annotated with `@PostConstruct` to populate the `students` list only once during bean initialization.
    - Update the `getStudents()` method to return the pre-loaded `students` list instead of creating and populating a new list on every request.

--- 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
+++ 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
@@ -1,6 +1,7 @@
 package dev.alimansour.demo.rest
 
 import dev.alimansour.demo.entity.Student
+import jakarta.annotation.PostConstruct
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,13 +10,22 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class StudentRestController {
 
+    private lateinit var students: MutableList<Student>
+
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    fun loadData() {
+        students = mutableListOf()
+        students.add(Student("Poornima", "Patel"))
+        students.add(Student("Mario", "Rossi"))
+        students.add(Student("Mary", "Smith"))
+    }
+
     // define endpoint for "/students" - return a list of Student
+
     @GetMapping("/students")
     fun getStudents(): List<Student> {
-        val students = mutableListOf<Student>()
-        students.add(Student("Poornima","Patel"))
-        students.add(Student("Mario","Rossi"))
-        students.add(Student("Mary","Smith"))
         return students
     }
 }
