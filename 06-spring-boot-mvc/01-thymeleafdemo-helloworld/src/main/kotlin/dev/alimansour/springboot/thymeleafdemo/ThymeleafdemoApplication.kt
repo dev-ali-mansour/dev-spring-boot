155163b3617b286ccd6ce2bf8c25305a6f76b094
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-17 16:20:46 +0300

    chore: Initialize Spring Boot Thymeleaf demo project
    Sets up a new Spring Boot application for demonstrating Thymeleaf
    with Spring Web MVC and Kotlin. Includes the basic project structure,
    Maven wrapper, pom.xml dependencies, and main application class.

---
+++ 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/kotlin/dev/alimansour/springboot/thymeleafdemo/ThymeleafdemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.alimansour.springboot.thymeleafdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafdemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThymeleafdemoApplication>(*args)
+}
