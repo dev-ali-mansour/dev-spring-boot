50b56bfb05bd2258f42d9ee78977b681adc6bcaa
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-19 16:24:25 +0300

    feat: Add StringTrimmerEditor to customer controller
    Register a StringTrimmerEditor in the CustomerController using @InitBinder to automatically trim leading and trailing whitespaces from String inputs. This ensures empty spaces are converted to null, preventing them from bypassing validation constraints.

--- 06-spring-boot-mvc/validationdemo/src/main/kotlin/dev/alimansour/springdemo/mvc/CustomerController.kt
+++ 06-spring-boot-mvc/validationdemo/src/main/kotlin/dev/alimansour/springdemo/mvc/CustomerController.kt
@@ -1,15 +1,24 @@
 package dev.alimansour.springdemo.mvc
 
 import jakarta.validation.Valid
+import org.springframework.beans.propertyeditors.StringTrimmerEditor
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.InitBinder
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class CustomerController {
+    @InitBinder
+    fun initBinder(dataBinder: WebDataBinder) {
+        val stringTrimmerEditor = StringTrimmerEditor(true)
+        dataBinder.registerCustomEditor(String::class.java, stringTrimmerEditor)
+    }
+
     @GetMapping("/")
     fun showForm(model: Model): String {
         model.addAttribute("customer", Customer())
@@ -21,6 +30,7 @@ class CustomerController {
         @Valid @ModelAttribute("customer") customer: Customer,
         bindingResult: BindingResult,
     ): String {
+        println("Last name: |${customer.lastName}|")
         if (bindingResult.hasErrors()) {
             return "customer-form"
         } else {
