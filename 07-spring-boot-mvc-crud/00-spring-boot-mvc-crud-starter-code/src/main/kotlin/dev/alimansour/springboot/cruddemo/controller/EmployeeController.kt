78b978a4ae9d6cca2f2bacf85a63b06d429d270c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-20 11:56:22 +0300

    feat: Add employee list view and controller mapping
    Adds the `/employees/list` endpoint to the `EmployeeController` to retrieve
    all employees from the service layer and populate the view model. Also
    updates the `list-employees.html` template with a Bootstrap-styled table
    using Thymeleaf to display the employee data.

--- 07-spring-boot-mvc-crud/00-spring-boot-mvc-crud-starter-code/src/main/kotlin/dev/alimansour/springboot/cruddemo/controller/EmployeeController.kt
+++ 07-spring-boot-mvc-crud/00-spring-boot-mvc-crud-starter-code/src/main/kotlin/dev/alimansour/springboot/cruddemo/controller/EmployeeController.kt
@@ -1,4 +1,21 @@
 package dev.alimansour.springboot.cruddemo.controller
 
-class EmployeeController {
+import dev.alimansour.springboot.cruddemo.service.EmployeeService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/employees")
+class EmployeeController(employeesService: EmployeeService, private val employeeService: EmployeeService) {
+        @GetMapping("/list")
+    fun listEmployees(model: Model): String {
+        val employees = employeeService.findAll()
+
+        model.addAttribute("employees", employees)
+
+        return "list-employees"
+    }
+
 }
