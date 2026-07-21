3de732e2b114e5b915aab108f60656c39a1c7dde
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-21 14:03:33 +0300

    feat: Add home page view and controller mapping
    Create the DemoController to handle requests to the root path and serve
    the initial Thymeleaf home template for the Spring Security demo.

---
+++ 08-spring-boot-mvc-security/demosecurity/src/main/kotlin/dev/alimansour/springboot/demosecurity/controller/DemoController.kt
@@ -0,0 +1,12 @@
+package dev.alimansour.springboot.demosecurity.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class DemoController {
+    @GetMapping("/")
+    fun showHome(): String {
+        return "home"
+    }
+}
