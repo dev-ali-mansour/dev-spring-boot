b25c7d9a67c8eeba2fa04d82b7bd63adeb4113eb
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-25 17:22:02 +0300

    feat: Add reusable @Pointcut declaration for DAO package
    Declare @Pointcut for all methods in dev.alimansour.aopdemo.dao package
    and update @Before advice to reference the pointcut method forDaoPackage().

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/kotlin/dev/alimansour/aopdemo/aspect/MyDemoLoggingAspect.kt
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/kotlin/dev/alimansour/aopdemo/aspect/MyDemoLoggingAspect.kt
@@ -2,12 +2,17 @@ package dev.alimansour.aopdemo.aspect
 
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
 class MyDemoLoggingAspect {
-    @Before("execution(* dev.alimansour.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* dev.alimansour.aopdemo.dao.*.*(..))")
+    fun forDaoPackage() {
+    }
+
+    @Before("forDaoPackage()")
     fun beforeAddAccountAdvice() {
         println("\n=====> Executing @Before advice on method")
     }
